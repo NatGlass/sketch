380f368e8e16ff665f5e129559f7883df69ed1c5
Visibyte <nathanglass26@gmail.com>
2023-11-06 16:47:02 +0000

    feat: generate random background color on hover

--- script.js
+++ script.js
@@ -7,7 +7,18 @@ for (let i = 0; i < NUMBER_OF_SQUARES; i++) {
   const SQUARE = document.createElement("div");
   GRID_CONTAINER.appendChild(SQUARE);
 
+  let color = generateRandomColor();
+
+  console.log(color);
+
   SQUARE.addEventListener("mouseover", (e) => {
-    e.currentTarget.style.background = "red";
+    e.currentTarget.style.background = `#${color}`;
+    e.currentTarget.style.border = `#${color}`;
   });
+}
+
+function generateRandomColor() {
+  let rgb = Math.floor(Math.random() * 999999);
+
+  return rgb;
 }
