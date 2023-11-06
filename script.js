7194b883d7b893fea17a6475d65a7ec6ef56f0ef
Visibyte <nathanglass26@gmail.com>
2023-11-06 16:35:46 +0000

    feat: change square background color on hover

--- script.js
+++ script.js
@@ -6,4 +6,8 @@ const GRID_CONTAINER = document.getElementById("grid-container");
 for (let i = 0; i < NUMBER_OF_SQUARES; i++) {
   const SQUARE = document.createElement("div");
   GRID_CONTAINER.appendChild(SQUARE);
+
+  SQUARE.addEventListener("mouseover", (e) => {
+    e.currentTarget.style.background = "red";
+  });
 }
