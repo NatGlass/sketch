db157e5d28a4835c732832be6079d56f4c5c1499
Visibyte <nathanglass26@gmail.com>
2023-11-06 15:50:03 +0000

    feat: create a div 256 times

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // 16x16 = 256
 const NUMBER_OF_SQUARES = 256;
 
+const GRID_CONTAINER = document.getElementById("grid-container");
+
+const SQUARE = document.createElement("div");
+
+for (let i = 0; i < NUMBER_OF_SQUARES; i++) {
+  GRID_CONTAINER.appendChild(SQUARE);
+}
